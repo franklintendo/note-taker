884bdbbeeb40af9c15ad74801b724c805800eddb
Frank Moran <info@francisfrankmoran.com>
2020-02-04 22:09:46 -0500

    comments

--- server.js
+++ server.js
@@ -25,31 +25,45 @@ app.get("/api/notes", function(req, res) {
 // The app works with the saveNote function 
 //that does an AJAX POST method in index.js
 app.post("/api/notes", (req, res) => {
+    // Contains the note object 
     const newNote = req.body;
+    // Place existing note array into variable
     let dbNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-
+    // Push new note into the note array
     dbNotes.push(newNote);
 
+    // Add a unique id to each array item
     for (var i = 0; i < dbNotes.length;i++) {
         dbNotes[i].id = i+1;
     }
 
+    // Turn the array into a string for storage
     const stringedNote = JSON.stringify(dbNotes);
 
+    // Write the DB JSON file with the new note included
     fs.writeFile("./db/db.json", stringedNote, "utf8", (err, data) => {
         if(err) throw err;
       console.log("success");
     });
+
+    // Send JSON response of updated array
     res.json(dbNotes);
 });
 
+// The app works with the deleteNote function 
+//that does an AJAX DELETE method in index.js
 app.delete('/api/notes/:id', function (req, res) {
     // console.log(req.params.id);
+    // Take the id of the note clicked and
+    // take the existing note array
     const idToDelete = parseInt(req.params.id);
     let dbJSON = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
 
-    console.log(dbJSON);
-    console.log(idToDelete);
+    // console.log(dbJSON);
+    // console.log(idToDelete);
+    // Loop through note array and check
+    // for id of note clicked and compare
+    // to each object id, remove offending object
     for (var i = 0; i < dbJSON.length;i++) {
         if (idToDelete === parseInt(dbJSON[i].id)) {
             dbJSON.splice(i, 1);
